c3ca2e5dca54dbf997ad17ecd5975468935a283c
marianovungel <vungem88@gmail.com>
2022-04-05 14:57:04 -0300

    Deploy

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ app.post('/images', upload.single('image'), async (req, res) => {
   res.send({imagePath: `/images/${result.Key}`})
 })
 
-app.listen(8080, () => console.log("listening on port 8080"))
+app.listen(process.env.PORT, () => console.log("listening on port 8080"))
